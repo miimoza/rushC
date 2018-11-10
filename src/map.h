709bbb86f639fd73e70e2cafffaa65882056b649
William Chow <william.chow@epita.fr>
2018-11-10 13:57:00 +0100

    Changed statics to pointers in parsing

--- src/map.h
+++ src/map.h
@@ -19,13 +19,13 @@ struct map
     int width;
     int height;
     int nbentities;
-    struct entity *entities
+    struct entity *entities;
     enum blocktype *block;
 };
 
-struct map *parse_map(char *path);
-int getwidth(char *path);
-int getheight(char *path);
-struct map **parse_maps(char *path);
+struct map *parse_map(char *path, char *texture_path);
+int get_width(char *path);
+int get_height(char *path);
+struct map **parse_maps(char **path, int size, char **texture_path);
 
 #endif /* !MAP_H_ */
