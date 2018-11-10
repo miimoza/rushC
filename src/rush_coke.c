d0a9f6d684b979bbf0f55a5a164db0bb84d8f4ba
Dorian Vinai <dorian.vinai@epita.fr>
2018-11-10 16:01:29 +0100

    Fixed game.c and main.

--- src/rush_coke.c
+++ src/rush_coke.c
@@ -4,32 +4,36 @@
 #include "game.h"
 #include "display.h"
 
-int main(int argc, char** argv)
+int main(void)
 {
     printf("test\n" );
 
     struct map *map = parse_map("maps/lvl1.ez", "maps/lvl1_textures/");
-    printf("test\n" );
     struct display *display = init_display(map->width, map->height);
-    printf("test\n" );
     load_textures(display, map->texture_path);
-    printf("test\n" );
-    display_map(display, map);
-    printf("test\n" );
-    SDL_Delay(10000);
-    printf("test\n" );
-/*
-    struct GameContext game = init_game();
+    //display_map(display, map);
+    //SDL_Delay(10000);
 
-    while (game.is_playing)
+    struct GameContext *game = init_game();
+
+    while (game->is_playing)
     {
+        display_map(display, map);
         //struct Input event = get_current_input();
         // compute new positions, sates, etc
         //update(&game, event);
         // draw the changes on the screen
-        render_frame(game);
-        SDL_Delay(3000);
+        //render_frame(game);
+        //SDL_Delay(3000);
+        SDL_Delay(10000);
+        game->is_playing = 0;
     }
-*/
+
+    //free
+    //free game
+    free(game);
+    //free display
+    //TODO
+
     return 0;
 }
