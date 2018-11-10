aff12e97febb00f38cf1821fa6d90baaac392415
William Chow <william.chow@epita.fr>
2018-11-10 17:01:42 +0100

    Implemented fucntion return pos x and y of entity.

--- src/map.c
+++ src/map.c
@@ -29,9 +29,55 @@ int get_height(char *path)
     return count;
 }
 
+int get_pos_x(char *path, char chr)
+{
+    FILE *file;
+    file = fopen(path, "r");
+    int count;
+    char c;
+    for (c = fgetc(file); c != chr; c = fgetc(file))
+    {
+        count++;
+    }
+    fclose(file);
+    return count;
+}
+
+int get_pos_y(char *path, char chr)
+{
+    FILE *file;
+    file = fopen(path, "r");
+    int count;
+    char c;
+    for (c = fgetc(file); c != chr; c = fgetc(file))
+    {
+        if (c == '\n')
+            count++;
+    }
+    fclose(file);
+    return count;
+}
+
+int get_entitynb(char *path)
+{
+    FILE *file;
+    file = fopen(path, "r");
+    int count = 0;
+    char c;
+    for (c = fgetc(file); c != EOF; c = fgetc(file))
+    {
+        if (c == 'p' || 'e' || 'g')
+            count++;
+    }
+    fclose(file);
+    return count;
+}
+
 struct map *parse_map(char *path, char *texture_path)
 {
     struct map *map = malloc(sizeof(struct map));
+    map->nbentities = 0;
+    map->entities = malloc(get_entitynb(path) * sizeof(struct entity));
     printf("spr\n");
     map->width = get_width(path);
     printf("width: %d\n", map->width);
@@ -55,6 +101,18 @@ struct map *parse_map(char *path, char *texture_path)
             map->block[i] = SPIKE;
         else if (c == 'd')
             map->block[i] = DBLOCK;
+        else if (c == 'p')
+        {
+            map->entities[map->nbentities++] = create_entity(PLAYER);
+        }
+        else if (c == 'e')
+        {
+            map->entities[map->nbentities++] = create_entity(ENEMY);
+        }
+        else if (c == 'g')
+        {
+            map->entities[map->nbentities++] = create_entity(GUN_PICKUP);
+        }
         else
             map->block[i] = AIR;
     }
@@ -82,4 +140,28 @@ int fti(float x)
 enum blocktype get_block(struct map *map, struct vector2 vec)
 {
     return map->block[fti(vec.x + 0.5f) + fti(vec.y) * map->width];
+}
+
+struct entity create_entity(enum entitytype)
+{
+    struct entity *player =
+    {
+        entitytype,
+        struct vector2 vec_pos =
+        {
+            get_pos_x(char *path, c),
+            get_pos_y(char *path, c)
+        }
+        struct vector2 vec_spd =
+        {
+            0,
+            0
+        };
+        struct vector2 vec_acc =
+        {
+            0,
+            0
+        }
+    };
+    return player;
 }
